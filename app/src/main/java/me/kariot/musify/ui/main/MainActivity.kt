2704531441b4f8ee166835374f2fee8300ed7fd2
Sreehari K <sreeharikariot@gmail.com>
2021-07-27 15:37:15 +0400

    project init

---
+++ app/src/main/java/me/kariot/musify/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package me.kariot.musify.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.kariot.musify.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
